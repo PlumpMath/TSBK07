ed4334595fc7a283d4b4f9578f59f52335870b7e
Armand Moulis <armmo193@olympen-12.edu.isy.liu.se>
2015-02-23 15:18:02 +0100

    Did an awful mouse look implementation.

--- lab3/main.c
+++ lab3/main.c
@@ -20,7 +20,7 @@
 #include "main.h"
 
 #define near 1.0
-#define far 30.0
+#define far 300.0
 #define right 0.5
 #define left -0.5
 #define top 0.5
@@ -39,9 +39,18 @@ mat4 transRoof;
 mat4 transBlade;
 mat4 transBalcony;
 
+mat4 lookMatrix;
+vec3 cameraPos;
+vec3 cameraDirection;
+vec3 cameraNormal;
 
 void init(void)
 {
+  cameraPos = (vec3){0.0f, 0.0f, 0.0f};
+  cameraDirection = (vec3){-0.0f, -10.0f, -20.0f};
+  cameraNormal = (vec3){0.0f, 1.0f, 0.0f};
+  lookMatrix = lookAtv(cameraPos, cameraDirection, cameraNormal);
+
 	dumpInfo();
 
 	walls = LoadModelPlus("windmill/windmill-walls.obj");
@@ -71,14 +80,12 @@ void init(void)
 	glUniform1i(glGetUniformLocation(program, "texUnit"), 0); // Texture unit 0
 
 	GLfloat projectionMatrix[] = {2.0f*near/(right-left), 0.0f, (right+left)/(right-left), 0.0f,
-															0.0f, 2.0f*near/(top-bottom), (top+bottom)/(top-bottom), 0.0f,
-															0.0f, 0.0f, -(far + near)/(far - near), -2*far*near/(far - near),
-															0.0f, 0.0f, -1.0f, 0.0f };
-	mat4 lookMatrix = lookAt(0.0f, 0.0f, 0.0f,
-													 -0.0f, -0.0f, -1.0f,
-													 0.0f, 1.0f, 0.0f);
+                                      0.0f, 2.0f*near/(top-bottom), (top+bottom)/(top-bottom), 0.0f,
+                                      0.0f, 0.0f, -(far + near)/(far - near), -2*far*near/(far - near),
+                                      0.0f, 0.0f, -1.0f, 0.0f };
+
 	glUniformMatrix4fv(glGetUniformLocation(program, "projectionMatrix"), 1, GL_TRUE, projectionMatrix);
-	glUniformMatrix4fv(glGetUniformLocation(program, "lookMatrix"), 1, GL_TRUE, lookMatrix.m);
+
 }
 
 void OnTimer(int value)
@@ -110,6 +117,9 @@ void display(void)
 		mat4 transform = Mult(transBlade, rotBlade);
 		drawObject(transform, blade);
 	}
+
+	glUniformMatrix4fv(glGetUniformLocation(program, "lookMatrix"), 1, GL_TRUE, lookMatrix.m);
+
 	drawObject(transWalls, walls);
 	drawObject(transRoof, roof);
 	drawObject(transBalcony, balcony);
@@ -131,13 +141,24 @@ int main(int argc, char *argv[])
 	glutCreateWindow ("Lab 3");
 	glutDisplayFunc(display);
 	initKeymapManager();
+	glutPassiveMotionFunc(handleMouse);
 	init ();
 	glutTimerFunc(16.7, &OnTimer, 0);
 	glutMainLoop();
-
+	
 }
 
-vec3 moveOnKeyInput(GLfloat x, GLfloat y, GLfloat z){
+void handleMouse(int x, int y) 
+{
+
+  cameraPos = (vec3) {cos((float)x / 400 * M_PI) * 20, sin((float)y / 400 * M_PI) * 20, cameraPos.z};
+  
+  lookMatrix = lookAtv(cameraPos, cameraDirection, cameraNormal);
+  
+}
+
+vec3 moveOnKeyInput(GLfloat x, GLfloat y, GLfloat z)
+{
   struct vec3 returnValue;
   returnValue.x = x;
   returnValue.y = y;
--- lab3/main.h
+++ lab3/main.h
@@ -7,3 +7,5 @@
 void drawObject(mat4 transform, Model* model);
 
 vec3 moveOnKeyInput(GLfloat x, GLfloat y, GLfloat z);
+
+void handleMouse(int x, int y);
